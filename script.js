601d1818d175439eb2032f4c3c20d2e8229cb9c6
Avik Roy <somavik99@gmail.com>
2024-02-08 18:01:30 +0530

    main

---
+++ script.js
@@ -0,0 +1,13 @@
+let TotalCount = document.getElementById("TotalCount");
+let WrittenText = document.getElementById("Written");
+let TextArea = document.getElementById("textarea");
+
+TextArea.addEventListener("keyup", function () {
+  TextCharacterUpdate();
+});
+
+function TextCharacterUpdate() {
+  TotalCount.innerText = TextArea.value.length;
+  WrittenText.innerText =
+    TextArea.getAttribute("maxlength") - TextArea.value.length;
+}
